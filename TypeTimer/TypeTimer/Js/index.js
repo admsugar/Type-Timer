c1a3ac392695b91f40b3b801191429645df200b8
Adam Sugarman <sugarmanah@g.cofc.edu>
2015-12-03 10:46:16 -0500

    Timer Initial

--- TypeTimer/TypeTimer/Js/index.js
+++ TypeTimer/TypeTimer/Js/index.js
@@ -37,7 +37,7 @@ function UpdatePrompt(lines) {
 //todo:
 function BeginTest() {
 
-    countdown();
+    initializeClock('clockdiv', deadline);
 }
 
 
@@ -50,12 +50,19 @@ function BeginTest() {
 var mins = 1;
 // calculate the seconds (don't change this! unless time progresses at a different speed for you...)
 var seconds = mins * 60;
+var begin = true;
 
 function countdown() {
     setTimeout(Decrement, 1000);
 }
 
 function Decrement() {
+
+    if (begin) {
+        begin = false;
+
+    }
+
     if (document.getElementById) {
         time = document.getElementById("timer");
         // if less than a minute remaining
@@ -77,3 +84,49 @@ function getseconds() {
     // take mins remaining (as seconds) away from total seconds remaining
     return secs - Math.round(mins * 60);
 }
+
+
+
+
+
+function getTimeRemaining(endtime) {
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    var seconds = Math.floor((t / 1000) % 60);
+    var minutes = Math.floor((t / 1000 / 60) % 60);
+    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    };
+}
+
+function initializeClock(id, endtime) {
+    var clock = document.getElementById(id);
+    var daysSpan = clock.querySelector('.days');
+    var hoursSpan = clock.querySelector('.hours');
+    var minutesSpan = clock.querySelector('.minutes');
+    var secondsSpan = clock.querySelector('.seconds');
+
+    function updateClock() {
+        var t = getTimeRemaining(endtime);
+
+        daysSpan.innerHTML = t.days;
+        hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+        minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+        secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+        if (t.total <= 0) {
+            clearInterval(timeinterval);
+        }
+    }
+
+    updateClock();
+    var timeinterval = setInterval(updateClock, 1000);
+}
+
+var deadline = 'December 31 2015 00:00:50 UTC+0200';
+initializeClock('clockdiv', deadline);
